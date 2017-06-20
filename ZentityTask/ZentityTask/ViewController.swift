1d15021941aa41a420d876e6131de1decb19791b
Roland Beke <beke@mofa.sk>
2017-06-20 23:11:25 +0200

    created singleView project

---
+++ ZentityTask/ZentityTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZentityTask
+//
+//  Created by Roland Beke on 20.6.17.
+//  Copyright © 2017 Roland Beke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
